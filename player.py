53f391f28d870657f13ce4c14f1e234ae1670f82
Čeněk Použník <potuznikcenek@gmail.com>
2024-04-07 12:54:53 +0200

    fixed error when bringing window to top

--- player.py
+++ player.py
@@ -1,7 +1,8 @@
 import vlc
 import multiprocessing
-import pyautogui
 from time import sleep
+import pygetwindow as gw
+import pywinauto
 
 def play_video(filename):
     player = vlc.MediaPlayer(filename)
@@ -15,13 +16,16 @@ def play_video(filename):
 def play_video_multiproc(filename) -> multiprocessing.Process:
     proc = multiprocessing.Process(target=play_video, args=(filename,))
     proc.start()
-    sleep(2)
-    bring_window_to_top('VLC (Direct3D11 output)')
+    sleep(5)
+    focus_to_window('VLC (Direct3D11 output)')
     return proc
 
-def bring_window_to_top(window_title):
-    window_pos = pyautogui.getWindowsWithTitle(window_title)
-    if len(window_pos) > 0:
-        window_pos[0].activate()
+
+def focus_to_window(window_title):
+    windows = gw.getWindowsWithTitle(window_title)
+    if len(windows) > 0:
+        window = windows[0]      
+        if window.isActive == False:
+            pywinauto.application.Application().connect(handle=window._hWnd).top_window().set_focus()
     else:
         print("Window not found.")
